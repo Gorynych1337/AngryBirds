dd680e958e12831bbb090f025b3548ad8e285cb4
Gorynych_0 <25215voe@mail.ru>
2024-10-20 22:56:14 +0300

    Font, Picks

--- Assets/Scripts/HUD/Hud.cs
+++ Assets/Scripts/HUD/Hud.cs
@@ -42,6 +42,7 @@ public class Hud : MonoBehaviour
     public void ToMainMenuClick()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
     }
 
     public void RestartLevel()
